49641eec4f46dc6b1abd4c5d3f179f569ce9f5e6
Anteconix <magnofilho2323@gmail.com>
2023-11-10 20:11:55 -0300

    Adicionar arquivos de projeto.

---
+++ Sibi/Tela.cs
@@ -0,0 +1,25 @@
+﻿namespace Sibi
+{
+    class Tela
+{
+    // propriedades
+    ConsoleColor corTexto, corFundo;
+
+
+    // construtor
+    public Tela(ConsoleColor ct = ConsoleColor.White,
+                ConsoleColor cf = ConsoleColor.Black)
+    {
+        this.corFundo = cf;
+        this.corTexto = ct;
+
+        this.configurarTela();
+    }
+
+
+    public void configurarTela()
+    {
+        Console.BackgroundColor = this.corFundo;
+        Console.ForegroundColor = this.corTexto;
+        Console.Clear();
+    }
