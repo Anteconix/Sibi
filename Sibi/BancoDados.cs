3f469a264cfb1613a47fa7e1f7eb3c7fd18d1503
Anteconix <magnofilho2323@gmail.com>
2023-11-21 21:42:04 -0300

    Progresso 21/11

--- Sibi/BancoDados.cs
+++ Sibi/BancoDados.cs
@@ -22,7 +22,6 @@ namespace Sibi
         {
             // cria alguns registros para realizar permitir o uso rápido do sistema
 
-
             // adiciona alguns hoteis
             this.hoteis.Add(new Hotel("Mont'Rei", "Joinville"));
             this.hoteis.Add(new Hotel("Glorian Real", "Barra Velha"));
@@ -35,41 +34,47 @@ namespace Sibi
             this.hospedes.Add(new Hospede("Carlos Magno", "00000000000", "Rua Fulano de Tal, 101", "4799999-9999"));
             this.hospedes.Add(new Hospede("Bryann Lucas", "10000000000", "Rua Fulano de Tal, 102", "4799999-9991"));
             this.hospedes.Add(new Hospede("Rafael Pavesi", "20000000000", "Rua Fulano de Tal, 103", "4799999-9992"));
-
-
         }
-
 
         public int buscar(string onde, string oque)
         {
             int posicao = -1;
 
-            if (onde == "autor") posicao = this.autores.FindIndex(o => o.Codigo == oque);
-            if (onde == "autor") posicao = this.autores.FindIndex(o => o.Codigo == oque);
-            if (onde == "autor") posicao = this.autores.FindIndex(o => o.Codigo == oque);
-            if (onde == "autor") posicao = this.autores.FindIndex(o => o.Codigo == oque);
-
+            if (onde == "hospede") posicao = this.hospedes.FindIndex(o => o.Codigo == oque);
+            if (onde == "hotel") posicao = this.hoteis.FindIndex(o => o.Codigo == oque);
+            if (onde == "pagamento") posicao = this.pagamentos.FindIndex(o => o.Codigo == oque);
+            if (onde == "quarto") posicao = this.quartos.FindIndex(o => o.Codigo == oque);
+            if (onde == "redehotel") posicao = this.redeshoteis.FindIndex(o => o.Codigo == oque);
+            if (onde == "reserva") posicao = this.reservas.FindIndex(o => o.Codigo == oque);
+            if (onde == "servicoextra") posicao = this.servicosextras.FindIndex(o => o.Codigo == oque);
 
             return posicao;
         }
 
-
         public object recuperar(string onde, int qual)
         {
             Object obj = null;
-            if (onde == "editora") obj = this.editoras[qual];
-            if (onde == "autor") obj = this.autores[qual];
-            if (onde == "livro") obj = this.livros[qual];
-            if (onde == "anotacao") obj = this.anotacoes[qual];
+            if (onde == "hospede") obj = this.hospedes[qual];
+            if (onde == "hotel") obj = this.hoteis[qual];
+            if (onde == "pagamento") obj = this.pagamentos[qual];
+            if (onde == "quarto") obj = this.quartos[qual];
+            if (onde == "redehotel") obj = this.redeshoteis[qual];
+            if (onde == "reserva") obj = this.reservas[qual];
+            if (onde == "servicoextra") obj = this.servicosextras[qual];
             return obj;
         }
 
 
         public void gravar(string onde, Object oque)
         {
-            if (onde == "editora") this.editoras.Add((Editora)oque);
-            if (onde == "autor") this.autores.Add((Autor)oque);
-            if (onde == "livro") this.livros.Add((Livro)oque);
+            if (onde == "hospede") this.hospedes.Add((Hospede)oque);
+            if (onde == "hotel") this.hoteis.Add((Hotel)oque);
+            if (onde == "pagamento") this.pagamentos.Add((Pagamento)oque);
+            if (onde == "quarto") this.quartos.Add((Quarto)oque);
+            if (onde == "redehotel") this.redeshoteis.Add((RedeHotel)oque);
+            if (onde == "reserva") this.reservas.Add((Reserva)oque);
+            if (onde == "servicoextra") this.servicosextras.Add((ServicoExtra)oque);
+
         }
 
 
